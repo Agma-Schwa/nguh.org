d9c0da9fcd4c953cd3d6fe4c26c58ceb0fcbf693
Sirraide <aeternalmail@gmail.com>
2024-06-21 20:37:45 +0200

    [GBH] Update description

--- src/routes/gambian_holiday/+page.svelte
+++ src/routes/gambian_holiday/+page.svelte
@@ -12,11 +12,16 @@
         <p>
             The Gambian Holiday Wiki is a rabbithole wiki-formatted story by
             Agma Schwa (King Horror) and Suzudo. The project began in late 2018
-            as a joke and has been on hiatus since March of 2020 in preparation
-            for the moving of the site from its current location on Fandom to its
-            future location as a subdomain of nguh.org. Much of the content is
-            going to be revamped, so the original Fandom continuity may be slightly
-            invalidated by the time it arrives here on nguh.org.
+            as a joke and had been on hiatus since March of 2020 in preparation
+            for the moving of the site from its past location on Fandom to its
+            current location as a subdomain of nguh.org.
+        </p>
+        <p>
+            Some of the content ended up being revamped, and the original Fandom
+            continuity had been somewhat invalidated by the time of its arrival
+            here on nguh.org; the move was completed in June of 2024, with Ætérnal
+            joining as an editor and new entries continuing to be added to the wiki
+            ever since.
         </p>
         <a href="https://gambianholiday.nguh.org/w/index.php?title=Main_Page" class="link-button"
            style="font-size: 1.5em; max-width: 30rem; height: 4rem; line-height: 4rem; padding: .4rem; margin: 3rem auto 1rem auto">
