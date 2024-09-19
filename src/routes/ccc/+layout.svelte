a5046e4502753236713e414e1e824c7aa3d5b9ec
Sirraide <aeternalmail@gmail.com>
2024-09-19 12:46:10 +0200

    [CCC] Further improve login banner style

--- src/routes/ccc/+layout.svelte
+++ src/routes/ccc/+layout.svelte
@@ -18,9 +18,6 @@
         height: 2rem;
         line-height: 2rem;
 
-        background: var(--accentblack);
-        border-radius: 0 0 5rem 5rem;
-
         font-size: var(--text-size);
         text-align: center;
         color: white;
@@ -36,13 +33,30 @@
         }
     }
 }
+
+// If the screen is large enough, use a hack with borders to create sharp corners.
+@media (min-width: 500px) {
+    #login-info-content {
+        border-top: 2rem solid var(--accentblack);
+        border-right: 2.5rem solid transparent;
+        border-left: 2.5rem solid transparent;
+        div { transform: translateY(-2rem); }
+    }
+}
+
+// On tiny screens, just render a box.
+@media (max-width: 500px) {
+    #login-info-content div { background: var(--accentblack); }
+}
 </style>
 
 {#if $page.data?.session?.user}
 <div id="login-info">
     <div id="login-info-content">
-        Logged in as <span class="name">{$page.data.session.user.name}</span> •
-        <a href="/ccc/login">Log out</a>
+        <div>
+            Logged in as <span class="name">{$page.data.session.user.name}</span> •
+            <a href="/ccc/login">Log out</a>
+        </div>
     </div>
 </div>
 {/if}
