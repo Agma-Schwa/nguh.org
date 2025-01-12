4ccb4f104deabb73a0da541ce77f38a0766784cd
Sirraide <aeternalmail@gmail.com>
2025-01-12 13:53:57 +0100

    [Dialog] Finish migrating the single file dialog

--- src/lib/components/dialog/SingleFileDialog.svelte
+++ src/lib/components/dialog/SingleFileDialog.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-    import FileDialogBase from "$lib/components/dialog/FileDialogBase.svelte";
-    import {type FileDialogData, FileDialogResult, type FileType} from "$lib/js/dialog2";
+    import {FileDialogResult, type FileType} from "$lib/js/dialog2";
     import ErrorDialog from "$lib/components/dialog/ErrorDialog.svelte";
+    import Dialog from "$lib/components/dialog/Dialog.svelte";
 
-    let textbox: HTMLInputElement
-    let base: FileDialogBase
-    let file: string | File
-    let is_file: boolean
+    let the_dialog: Dialog
+    let input: HTMLInputElement
+    let file_or_url = $state('')
+    let uploaded_file_data: File | null  | undefined = $state(null)
     let error: Error | string = $state('')
     let error_dialog: ErrorDialog
 
     interface Props {
         title: string
         description?: string,
-        type?: FileType,
+        type: FileType,
         preserve_extern_urls?: boolean
     }
 
@@ -24,23 +24,18 @@
         preserve_extern_urls = false,
     }: Props = $props()
 
-    $effect(() => {
-        file = ''
-        is_file = false
-    })
-
-    export function open(): Promise<FileDialogResult> { return base.open() }
+    export function open(): Promise<FileDialogResult> { return the_dialog.open() }
 
     async function Accept() {
         try {
-            if (is_file) return base.submit(await FileDialogResult.Resolve(file as File, type), type)
-            if (textbox.value === '') return // Nothing in the text box.
-            if (preserve_extern_urls) return base.submit(file, 'text', true)
-            const res = await fetch(textbox.value)
+            if (uploaded_file_data) return Submit(await FileDialogResult.Resolve(uploaded_file_data, type), type)
+            if (file_or_url === '') return // Nothing in the text box.
+            if (preserve_extern_urls) return Submit(file_or_url, 'text', true)
+            const res = await fetch(file_or_url)
             switch (type) {
-                case 'raw': return base.submit(await res.blob(), type)
-                case 'text': return base.submit(await res.text(), type)
-                case 'json': return base.submit(await res.json(), type)
+                case 'raw': return Submit(await res.blob(), type)
+                case 'text': return Submit(await res.text(), type)
+                case 'json': return Submit(await res.json(), type)
             }
         } catch (e: any) {
             error = e
@@ -49,37 +44,61 @@
     }
 
     function UpdateInput(event: Event) {
-        // Set the textbox value to the input filename.
-        const input = base.get_input()
-        textbox.value = input.value.slice(input.value.lastIndexOf('\\') + 1)
-
         // Grab the file.
-        if (!event.target) return
-        file = (<HTMLInputElement>event.target)?.files?.item(0) ?? ''
-        if (file === '') return
-        is_file = true
+        const file = (<HTMLInputElement>event.target)?.files?.item(0)
+        if (!file) return
+        uploaded_file_data = file
+        file_or_url = input.value.slice(input.value.lastIndexOf('\\') + 1)
+    }
 
-        // And disable editing the text box.
-        textbox.disabled = true
+    function Submit(
+        data: string | object | File,
+        type: FileType,
+        is_url: boolean = false
+    ) {
+        the_dialog.resolve(new FileDialogResult(data, type, is_url))
     }
 </script>
 
 <ErrorDialog {error} bind:this={error_dialog} />
 
-<FileDialogBase
+<!--{#if multiple}
+    <p class="ml-8" bind:this={box}></p>-->
+{#snippet content()}
+    <p>{description}</p>
+    <div class="flex file-input-control">
+        <input type="file" bind:this={input} onchange={UpdateInput}>
+        <input
+            type="text"
+            placeholder="Enter a URL or click 'Browse Files'"
+            disabled={!!uploaded_file_data}
+            bind:value={file_or_url}
+        >
+    </div>
+{/snippet}
+
+{#snippet controls()}
+    <button onclick={() => input.click()}>Browse Files</button>
+    <button onclick={Accept} disabled={file_or_url === ''}>OK</button>
+    <button onclick={() => the_dialog.reject('User pressed Cancel')}>Cancel</button>
+{/snippet}
+
+<Dialog
+    bind:this={the_dialog}
     {title}
-    {description}
-    multiple={false}
-    on_file_selected={UpdateInput}
-    ok_callback={Accept}
-    bind:this={base}
->   <input type="text" placeholder="Enter a URL or click 'Browse Files'" bind:this={textbox}>
-</FileDialogBase>
+    {content}
+    {controls}
+/>
 
 <style lang="scss">
-    input[type=text] {
-        font-size: var(--text-size);
-        width: 100%;
-        padding: 0;
+    .file-input-control {
+        gap: .55rem;
+        button { width: 11.5rem; }
+        input[type=file] { display: none; }
+        input[type=text] {
+            font-size: var(--text-size);
+            width: 100%;
+            padding: 0;
+        }
     }
 </style>
