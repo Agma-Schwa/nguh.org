6fbd2c791af5ec311ece8d9e73be5109b57e671f
Sirraide <aeternalmail@gmail.com>
2025-01-19 04:53:22 +0100

    [CSS] More cleanup

--- src/routes/tools/+page.svelte
+++ src/routes/tools/+page.svelte
@@ -12,79 +12,67 @@
 </section>
 
 <Stripe>List of Tools</Stripe>
-<section>
-    <h3>
-        The following is a list of conlanging tools that I have reviewed, and/or will be reviewing in the future, with
-        associated links to said videos
-    </h3>
+<section class='[&>h4]:text-left'>
+    <div class='w-full mb-8'>
+        <h3 class='w-2/3 mx-auto text-center'>
+            The following is a list of conlanging tools that I have reviewed, and/or will be reviewing in the future, with
+            associated links to said videos
+        </h3>
+    </div>
 
-    <h4 class="text-left">SCA++ - Sound Change Applier (New)</h4>
+    <h4>Lexurgy — Sound Change Applier</h4>
     <ul>
-        <li><a href="/tools/sca">https://www.nguh.org/tools/sca</a></li>
-        <li><a href="https://www.youtube.com/watch?v=5q8itgoe9hI">https://www.youtube.com/watch?v=5q8itgoe9hI</a></li>
+        <li><a href='https://lexurgy-app.vercel.app/sc'>https://lexurgy-app.vercel.app/sc</a></li>
     </ul>
 
-    <h4 class="text-left">Awkwords (Lexicon Generator)</h4>
-    <ul>
-        <li><a href="http://akana.conlang.org/tools/awkwords/">http://akana.conlang.org/tools/awkwords/</a></li>
-        <li><a href="https://www.youtube.com/watch?v=feVjB66yKlk">https://www.youtube.com/watch?v=feVjB66yKlk</a></li>
-    </ul>
-
-
-    <h4 class="text-left">GenGo (Lexicon Generator)</h4>
-    <ul>
-        <li><a href="https://collinbrennan.github.io/GenGo/index.html">https://collinbrennan.github.io/GenGo/index.html</a></li>
-        <li><a href="https://www.youtube.com/watch?v=Whz6hNAGM9k">https://www.youtube.com/watch?v=Whz6hNAGM9k</a></li>
-    </ul>
-
-    <h4 class="text-left">SCA² – Sound Change Applier (Phonological Change Generator)</h4>
+    <h4>SCA² – Sound Change Applier </h4>
     <ul>
         <li><a href="http://zompist.com/sca2.html">http://zompist.com/sca2.html</a></li>
         <li><a href="https://www.youtube.com/watch?v=AYktVyyha3Q">https://www.youtube.com/watch?v=AYktVyyha3Q</a></li>
     </ul>
 
-    <h4 class="text-left">Microsoft Keyboard Layout Creator (MSKLC) Version 1.4 (Custom Keyboard Maker for Windows)</h4>
+    <h4>Microsoft Keyboard Layout Creator (MSKLC) Version 1.4 (Custom Keyboard Maker for Windows)</h4>
     <ul>
         <li><a href="https://www.microsoft.com/en-us/download/details.aspx?id=102134">https://www.microsoft.com/en-us/download/details.aspx?id=102134</a></li>
         <li><a href="https://www.youtube.com/watch?v=x2j0VWmPbnk">https://www.youtube.com/watch?v=x2j0VWmPbnk</a></li>
     </ul>
 
-    <h4 class="text-left">Minecraft Custom Language Tutorial</h4>
+    <h4>Minecraft Custom Language Tutorial</h4>
     <ul>
         <li><a href="https://www.youtube.com/watch?v=moq_bNdGnoQ">https://www.youtube.com/watch?v=moq_bNdGnoQ</a></li>
     </ul>
 
-    <h4 class="text-left">Conlang Syntax Test Cases (Grammar and Syntax Situations)</h4>
+    <h4>Conlang Syntax Test Cases (Grammar and Syntax Situations)</h4>
     <ul>
         <li><a href="https://cofl.github.io/conlang/resources/mirror/conlang-syntax-test-cases.html">https://cofl.github.io/conlang/resources/mirror/conlang-syntax-test-cases.html</a></li>
         <li><a href="https://www.youtube.com/watch?v=4AlXcvDh7B4">https://www.youtube.com/watch?v=4AlXcvDh7B4</a></li>
     </ul>
 
-    <h4 class="text-left">Keyman Custom Keyboard Developer Tool</h4>
+    <h4>Keyman Custom Keyboard Developer Tool</h4>
     <ul>
         <li><a href="https://keyman.com/developer/">https://keyman.com/developer/</a></li>
         <li><a href="https://www.youtube.com/watch?v=_YaH-9o5KJk">https://www.youtube.com/watch?v=_YaH-9o5KJk</a></li>
     </ul>
 
-    <h4 class="text-left">Vulgarlang</h4>
+    <h4>Vulgarlang</h4>
     <ul>
         <li><a href="https://www.vulgarlang.com/">https://www.vulgarlang.com/</a></li>
         <li><a href="https://www.youtube.com/watch?v=vSkfcr7xm0U">https://www.youtube.com/watch?v=vSkfcr7xm0U</a></li>
     </ul>
 
-    <h4 class="text-left">Memrise</h4>
+    <h4>Memrise</h4>
     <ul>
         <li><a href="https://www.memrise.com/">https://www.memrise.com/</a></li>
         <li><a href="https://www.youtube.com/watch?v=--uk-gSqQ_E">https://www.youtube.com/watch?v=--uk-gSqQ_E</a></li>
     </ul>
 
-    <h4 class="text-left">Conworkshop</h4>
+    <h4>Conworkshop</h4>
     <ul>
         <li><a href="https://conworkshop.com/">https://conworkshop.com/</a></li>
         <li><a href="https://youtu.be/6QFSJxoJ-RM">https://youtu.be/6QFSJxoJ-RM</a></li>
     </ul>
 
-    <h4 class="text-left">The GUMSMAQQER</h4>
+    <h4>The GUMSMAQQER</h4>
     <ul>
         <li><a href="https://github.com/Sirraide/gumsmaqqer">https://github.com/Sirraide/gumsmaqqer</a></li>
     </ul>
