2976e478cc8e173db97ed9328ea20d042ed8d27e
Sirraide <aeternalmail@gmail.com>
2026-05-20 23:40:16 +0200

    [Dictionary] Add custom macro handler for Elvish
    Fixes Agma-Schwa/conlanging#4

--- src/routes/languages/elvish/dictionary/+page.svelte
+++ src/routes/languages/elvish/dictionary/+page.svelte
@@ -2,16 +2,33 @@
     import Page from '$lib/components/Page.svelte';
     import type {PageProps} from './$types';
     import Dictionary from '$lib/components/dictionary/Dictionary.svelte';
-    import {ParseDictionary} from '$lib/js/dictionary';
+    import {type Dictionary as Dict, ParseDictionary} from '$lib/js/dictionary';
+    import type {Snippet} from 'svelte';
 
     let { data }: PageProps = $props();
     const generator = $derived(await ParseDictionary(data.dict))
 </script>
 
 <Page name="Elvish Dictionary" banner={false} />
+
+{#snippet CustomMacroHandler(node: Dict.CustomMacroNode, render_callback: Snippet<[Dict.Node]>)}
+    {#if node.custom_macro.name === 'santaa' && node.custom_macro.args}
+        Santaa
+        <span class='lemma'>{@render render_callback(node.custom_macro.args[0])}</span>
+    {:else}
+        &lt;UNSUPPORTED CUSTOM MACRO: {node.custom_macro.name}&gt;
+    {/if}
+{/snippet}
+
 <Dictionary
     CustomSearchHandler={() => null}
+    {CustomMacroHandler}
     dict={generator}
     lang_code={'el'}
     search_example={'sánshų́'}
 />
+
+<style lang='scss'>
+    @use "$lib/css/dictionary" as *;
+    .lemma { @include word-format; }
+</style>
